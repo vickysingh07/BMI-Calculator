2cee72eabfb92c243e5dcd3adba7cb8bda7c586e
Vicky <vicky1910098@akgec.ac.in>
2020-09-28 00:55:24 +0530

    eleventh C

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -86,17 +86,28 @@ class _InputPageState extends State<InputPage> {
                     Text("cm", style: labelTextStyle)
                   ],
                 ),
-                Slider(
-                  value: height.toDouble(),
-                  min: 120,
-                  max: 220,
-                  activeColor: Color(0xFFEB1555),
-                  inactiveColor: Color(0xFF8D8E98),
-                  onChanged: (double newValue) {
-                    setState(() {
-                      height = newValue.round();
-                    });
-                  },
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0)),
+                  child: Slider(
+                    value: height.toDouble(),
+                    min: 120,
+                    max: 220,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
+                  ),
                 )
               ],
             ),
