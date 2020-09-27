d3162b8a608dd0a62579b29016bd3c8323fc6c00
Vicky <vicky1910098@akgec.ac.in>
2020-09-27 14:14:42 +0530

    fifth co

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome/flutte.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'InputPage.dart';
+import 'dart:ui';
 
 void main() => runApp(BMICalculator());
 
@@ -7,6 +8,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
