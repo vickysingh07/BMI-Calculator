685b58b21dade140e49f2be5aad4d63731aa989b
Vicky <vicky1910098@akgec.ac.in>
2020-09-28 22:24:40 +0530

    fourteen C

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,8 +1,31 @@
+import 'package:bmi_calculator/contstants.dart';
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
 
 class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Result',
+                style: TitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(colour: activeCardColour),
+          )
+        ],
+      ),
+    );
   }
 }
